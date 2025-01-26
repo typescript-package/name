91b9af7988477a46b13cdd415b5d1aa4626c9f93
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-26 21:05:14 +0000

    refactor: update `Prefix` and `Suffix` by adding the `Value` generic type variable.

--- src/affix/lib/prefix.class.ts
+++ src/affix/lib/prefix.class.ts
@@ -1,9 +1,18 @@
 // Class.
 import { Affix } from "./affix.abstract";
 /**
- * @description
+ * @description A class to manage prefixes that can be applied to strings.
  * @export
  * @class Prefix
- * @extends {Affix}
+ * @template {string} [Value=string] The type of prefix constrained by the `string`.
+ * @extends {Affix<Value>}
  */
-export class Prefix extends Affix {}
+export class Prefix<Value extends string = string> extends Affix<Value> {
+  /**
+   * @inheritdoc
+   * @public
+   * @static
+   * @type {RegExp}
+   */
+  public static override filter: RegExp = super.filter;
+}
--- src/affix/lib/suffix.class.ts
+++ src/affix/lib/suffix.class.ts
@@ -1,9 +1,24 @@
 // Class.
 import { Affix } from "./affix.abstract";
 /**
- * @description
  * @export
  * @class Suffix
  * @extends {Affix}
  */
-export class Suffix extends Affix {}
+
+/**
+ * @description A class to manage suffixes that can be applied to strings.
+ * @export
+ * @class Suffix
+ * @template {string} [Value=string] The type of suffix constrained by the `string`.
+ * @extends {Affix<Value>}
+ */
+export class Suffix<Value extends string = string> extends Affix<Value> {
+    /**
+   * @inheritdoc
+   * @public
+   * @static
+   * @type {RegExp}
+   */
+  public static override filter: RegExp = super.filter;
+}
