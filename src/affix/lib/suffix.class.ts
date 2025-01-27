d662b2844ce1398ded4fd02d9c22308db9a76e23
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-27 01:35:45 +0000

    refactor(Suffix): add static method sanitize and set the default.

--- src/affix/lib/suffix.class.ts
+++ src/affix/lib/suffix.class.ts
@@ -1,12 +1,6 @@
 // Class.
 import { Affix } from "./affix.abstract";
 /**
- * @export
- * @class Suffix
- * @extends {Affix}
- */
-
-/**
  * @description A class to manage suffixes that can be applied to strings.
  * @export
  * @class Suffix
@@ -14,11 +8,21 @@ import { Affix } from "./affix.abstract";
  * @extends {Affix<Value>}
  */
 export class Suffix<Value extends string = string> extends Affix<Value> {
+  /**
+   * @description Sanitizes the suffix with a `filter`.
+   * @public
+   * @param {string} value 
+   * @param {RegExp} [filter=Suffix.filter] 
+   * @returns {string} 
+   */
+  public static sanitize(value: string, filter: RegExp = Suffix.filter): string {
+    return value.replace(filter, '');
+  }
     /**
    * @inheritdoc
    * @public
    * @static
    * @type {RegExp}
    */
-  public static override filter: RegExp = super.filter;
+  public static override filter: RegExp = /[^a-zA-Z0-9$_-]/g;
 }
